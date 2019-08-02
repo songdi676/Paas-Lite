73d8428429ed4a4ee829cc49a3b1be2f81855636
gina00 <584525974@qq.com>
2019-08-02 17:31:44 +0800

    更新菜单管理布局

--- webapp/src/views/admin/menu/components/element.vue
+++ webapp/src/views/admin/menu/components/element.vue
@@ -1,107 +1,131 @@
 <template>
   <div class="app-container calendar-list-container">
     <div class="filter-container">
-      <el-input @keyup.enter.native="handleFilter" style="width: 200px;" class="filter-item" placeholder="资源名称" v-model="listQuery.name"> </el-input>
+      <el-input
+        @keyup.enter.native="handleFilter"
+        style="width: 200px;"
+        class="filter-item"
+        placeholder="资源名称"
+        v-model="listQuery.name"
+      ></el-input>
       <el-button class="filter-item" type="primary" v-waves icon="search" @click="handleFilter">搜索</el-button>
-      <el-button class="filter-item" v-if="menuManager_btn_element_add" style="margin-left: 10px;" @click="handleCreate" type="primary" icon="edit">添加</el-button>
+      <el-button
+        class="filter-item"
+        v-if="menuManager_btn_element_add"
+        style="margin-left: 10px;"
+        @click="handleCreate"
+        type="primary"
+        icon="edit"
+      >添加</el-button>
     </div>
-    <el-table :key='tableKey' :data="list" v-loading.body="listLoading" border fit highlight-current-row style="width: 100%">
+    <el-table
+      :key="tableKey"
+      :data="list"
+      v-loading.body="listLoading"
+      border
+      fit
+      highlight-current-row
+      style="width: 100%"
+    >
       <el-table-column align="center" label="id" width="65">
         <template scope="scope">
           <span>{{scope.row.id}}</span>
-</template>
-
-</el-table-column>
-    <el-table-column width="200px" align="center" label="资源编码"><template scope="scope">
-  <span>
-    {{scope.row.code}}</span>
-</template>
-
-</el-table-column>
-    <el-table-column width="200px" align="center" label="资源类型"><template scope="scope">
-  <span>
-    {{scope.row.type}}</span>
-</template>
-
-</el-table-column>
-    <el-table-column width="200px" align="center" label="资源名称"><template scope="scope">
-  <span>
-    {{scope.row.name}}</span>
-</template>
-
-</el-table-column>
-    <el-table-column width="200px" align="center" label="资源地址"><template scope="scope">
-  <span>
-    {{scope.row.uri}}</span>
-</template>
-</el-table-column>
-    <el-table-column width="200px" align="center" label="资源请求类型"><template scope="scope">
-  <span>
-    {{scope.row.method}}</span>
-</template>
-
-</el-table-column>
-    <el-table-column width="200px" align="center" label="描述"><template scope="scope">
-  <span>
-    {{scope.row.description}}</span>
-</template>
-
-</el-table-column>
-    <el-table-column fixed="right" align="center" label="操作" width="150"><template scope="scope">
-  <el-button v-if="menuManager_btn_element_edit" size="small" type="success" @click="handleUpdate(scope.row)">
-    编辑
-  </el-button>
-  <el-button v-if="menuManager_btn_element_del" size="small" type="danger" @click="handleDelete(scope.row)">删除
-  </el-button>
-</template>
-
-</el-table-column>
-  </el-table>
-  <div v-show="!listLoading" class="pagination-container">
-    <el-pagination @size-change="handleSizeChange" @current-change="handleCurrentChange" :current-page.sync="listQuery.page" :page-sizes="[10,20,30, 50]" :page-size="listQuery.limit" layout="total, sizes, prev, pager, next, jumper" :total="total"> </el-pagination>
-  </div>
-  <el-dialog :title="textMap[dialogStatus]" :visible.sync="dialogFormVisible">
-    <el-form :model="form" :rules="rules" ref="form" label-width="100px">
-      <el-form-item label="资源编码" prop="code">
-        <el-input v-model="form.code" placeholder="请输入资源编码"></el-input>
-      </el-form-item>
-      <el-form-item label="资源类型" prop="type">
-         <el-select class="filter-item" v-model="form.type" placeholder="请输入资源类型">
-          <el-option v-for="item in  typeOptions" :key="item" :label="item" :value="item"> </el-option>
-        </el-select>
-      </el-form-item>
-      <el-form-item label="资源名称" prop="name">
-        <el-input v-model="form.name" placeholder="请输入资源名称"></el-input>
-      </el-form-item>
-      <el-form-item label="资源地址" prop="uri">
-        <el-input v-model="form.uri" placeholder="请输入资源地址"></el-input>
-      </el-form-item>
-      <el-form-item label="资源请求类型" prop="method">
-        <el-select class="filter-item" v-model="form.method" placeholder="请输入资源请求类型">
-          <el-option v-for="item in  methodOptions" :key="item" :label="item" :value="item"> </el-option>
-        </el-select>
-      </el-form-item>
-    </el-form>
-    <div slot="footer" class="dialog-footer">
-      <el-button @click="cancel('form')">取 消</el-button>
-      <el-button v-if="dialogStatus=='create'" type="primary" @click="create('form')">确 定</el-button>
-      <el-button v-else type="primary" @click="update('form')">确 定</el-button>
+        </template>
+      </el-table-column>
+      <el-table-column width="200px" align="center" label="资源编码">
+        <template scope="scope">
+          <span>{{scope.row.code}}</span>
+        </template>
+      </el-table-column>
+      <el-table-column width="200px" align="center" label="资源类型">
+        <template scope="scope">
+          <span>{{scope.row.type}}</span>
+        </template>
+      </el-table-column>
+      <el-table-column width="200px" align="center" label="资源名称">
+        <template scope="scope">
+          <span>{{scope.row.name}}</span>
+        </template>
+      </el-table-column>
+      <el-table-column width="200px" align="center" label="资源地址">
+        <template scope="scope">
+          <span>{{scope.row.uri}}</span>
+        </template>
+      </el-table-column>
+      <el-table-column width="200px" align="center" label="资源请求类型">
+        <template scope="scope">
+          <span>{{scope.row.method}}</span>
+        </template>
+      </el-table-column>
+      <el-table-column width="200px" align="center" label="描述">
+        <template scope="scope">
+          <span>{{scope.row.description}}</span>
+        </template>
+      </el-table-column>
+      <el-table-column fixed="right" align="center" label="操作" width="150">
+        <template scope="scope">
+          <el-button
+            v-if="menuManager_btn_element_edit"
+            size="small"
+            type="success"
+            @click="handleUpdate(scope.row)"
+          >编辑</el-button>
+          <el-button
+            v-if="menuManager_btn_element_del"
+            size="small"
+            type="danger"
+            @click="handleDelete(scope.row)"
+          >删除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+    <div v-show="!listLoading" class="pagination-container">
+      <el-pagination
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+        :current-page.sync="listQuery.page"
+        :page-sizes="[10,20,30, 50]"
+        :page-size="listQuery.limit"
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="total"
+      ></el-pagination>
     </div>
-  </el-dialog>
-</div>
+    <el-dialog :title="textMap[dialogStatus]" :visible.sync="dialogFormVisible">
+      <el-form :model="form" :rules="rules" ref="form" label-width="100px">
+        <el-form-item label="资源编码" prop="code">
+          <el-input v-model="form.code" placeholder="请输入资源编码"></el-input>
+        </el-form-item>
+        <el-form-item label="资源类型" prop="type">
+          <el-select class="filter-item" v-model="form.type" placeholder="请输入资源类型">
+            <el-option v-for="item in  typeOptions" :key="item" :label="item" :value="item"></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="资源名称" prop="name">
+          <el-input v-model="form.name" placeholder="请输入资源名称"></el-input>
+        </el-form-item>
+        <el-form-item label="资源地址" prop="uri">
+          <el-input v-model="form.uri" placeholder="请输入资源地址"></el-input>
+        </el-form-item>
+        <el-form-item label="资源请求类型" prop="method">
+          <el-select class="filter-item" v-model="form.method" placeholder="请输入资源请求类型">
+            <el-option v-for="item in  methodOptions" :key="item" :label="item" :value="item"></el-option>
+          </el-select>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="cancel('form')">取 消</el-button>
+        <el-button v-if="dialogStatus=='create'" type="primary" @click="create('form')">确 定</el-button>
+        <el-button v-else type="primary" @click="update('form')">确 定</el-button>
+      </div>
+    </el-dialog>
+  </div>
 </template>
 
 <script>
-import {
-  page,
-  addObj,
-  getObj,
-  delObj,
-  putObj
-} from 'api/admin/element/index';
-import { mapGetters } from 'vuex';
+import { page, addObj, getObj, delObj, putObj } from "api/admin/element/index";
+import { mapGetters } from "vuex";
 export default {
-  name: 'element',
+  name: "element",
   // props: {
   //   menuId: {
   //     default: '1'
@@ -109,8 +133,8 @@ export default {
   // },
   data() {
     return {
-      methodOptions: ['GET', 'POST', 'PUT', 'DELETE'],
-      typeOptions: ['uri', 'button'],
+      methodOptions: ["GET", "POST", "PUT", "DELETE"],
+      typeOptions: ["uri", "button"],
       form: {
         code: undefined,
         type: undefined,
@@ -121,71 +145,77 @@ export default {
         description: undefined
       },
       rules: {
-        code: [{
-          required: true,
-          message: '请输入资源编码',
-          trigger: 'blur'
-        }
+        code: [
+          {
+            required: true,
+            message: "请输入资源编码",
+            trigger: "blur"
+          }
         ],
-        type: [{
-          required: true,
-          message: '请输入资源类型',
-          trigger: 'blur'
-        },
-        {
-          min: 2,
-          max: 20,
-          message: '长度在 2 到 20 个字符',
-          trigger: 'blur'
-        }
+        type: [
+          {
+            required: true,
+            message: "请输入资源类型",
+            trigger: "blur"
+          },
+          {
+            min: 2,
+            max: 20,
+            message: "长度在 2 到 20 个字符",
+            trigger: "blur"
+          }
         ],
-        name: [{
-          required: true,
-          message: '请输入资源名称',
-          trigger: 'blur'
-        },
-        {
-          min: 2,
-          max: 20,
-          message: '长度在 2 到 20 个字符',
-          trigger: 'blur'
-        }
+        name: [
+          {
+            required: true,
+            message: "请输入资源名称",
+            trigger: "blur"
+          },
+          {
+            min: 2,
+            max: 20,
+            message: "长度在 2 到 20 个字符",
+            trigger: "blur"
+          }
         ],
-        uri: [{
-          required: true,
-          message: '请输入',
-          trigger: 'blur'
-        },
-        {
-          min: 3,
-          max: 20,
-          message: '长度在 3 到 20 个字符',
-          trigger: 'blur'
-        }
+        uri: [
+          {
+            required: true,
+            message: "请输入",
+            trigger: "blur"
+          },
+          {
+            min: 3,
+            max: 20,
+            message: "长度在 3 到 20 个字符",
+            trigger: "blur"
+          }
         ],
-        method: [{
-          required: true,
-          message: '请输入资源请求类型',
-          trigger: 'blur'
-        },
-        {
-          min: 3,
-          max: 20,
-          message: '长度在 3 到 20 个字符',
-          trigger: 'blur'
-        }
+        method: [
+          {
+            required: true,
+            message: "请输入资源请求类型",
+            trigger: "blur"
+          },
+          {
+            min: 3,
+            max: 20,
+            message: "长度在 3 到 20 个字符",
+            trigger: "blur"
+          }
         ],
-        description: [{
-          required: true,
-          message: '请输入',
-          trigger: 'blur'
-        },
-        {
-          min: 3,
-          max: 20,
-          message: '长度在 3 到 20 个字符',
-          trigger: 'blur'
-        }
+        description: [
+          {
+            required: true,
+            message: "请输入",
+            trigger: "blur"
+          },
+          {
+            min: 3,
+            max: 20,
+            message: "长度在 3 到 20 个字符",
+            trigger: "blur"
+          }
         ]
       },
       list: null,
@@ -198,28 +228,32 @@ export default {
         menuId: this.menuId
       },
       dialogFormVisible: false,
-      dialogStatus: '',
+      dialogStatus: "",
       menuManager_btn_element_add: false,
       menuManager_btn_element_edit: false,
       menuManager_btn_element_del: false,
       menuId: -1,
       textMap: {
-        update: '编辑',
-        create: '创建'
+        update: "编辑",
+        create: "创建"
       },
       tableKey: 0
-    }
+    };
   },
   created() {
     this.getList();
-    this.menuManager_btn_element_add = this.elements['menuManager:btn_element_add'];
-    this.menuManager_btn_element_del = this.elements['menuManager:btn_element_del'];
-    this.menuManager_btn_element_edit = this.elements['menuManager:btn_element_edit'];
+    this.menuManager_btn_element_add = this.elements[
+      "menuManager:btn_element_add"
+    ];
+    this.menuManager_btn_element_del = this.elements[
+      "menuManager:btn_element_del"
+    ];
+    this.menuManager_btn_element_edit = this.elements[
+      "menuManager:btn_element_edit"
+    ];
   },
   computed: {
-    ...mapGetters([
-      'elements'
-    ])
+    ...mapGetters(["elements"])
   },
   methods: {
     getList() {
@@ -229,7 +263,7 @@ export default {
         this.list = response.data.rows;
         this.total = response.data.total;
         this.listLoading = false;
-      })
+      });
     },
     handleFilter() {
       this.getList();
@@ -244,27 +278,27 @@ export default {
     },
     handleCreate() {
       this.resetTemp();
-      this.dialogStatus = 'create';
+      this.dialogStatus = "create";
       this.dialogFormVisible = true;
     },
     handleUpdate(row) {
       getObj(row.id).then(response => {
         this.form = response.data;
         this.dialogFormVisible = true;
-        this.dialogStatus = 'update';
+        this.dialogStatus = "update";
       });
     },
     handleDelete(row) {
-      this.$confirm('此操作将永久删除, 是否继续?', '提示', {
-        confirmButtonText: '确定',
-        cancelButtonText: '取消',
-        type: 'warning'
+      this.$confirm("此操作将永久删除, 是否继续?", "提示", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "warning"
       }).then(() => {
         delObj(row.id).then(() => {
           this.$notify({
-            title: '成功',
-            message: '删除成功',
-            type: 'success',
+            title: "成功",
+            message: "删除成功",
+            type: "success",
             duration: 2000
           });
           const index = this.list.indexOf(row);
@@ -281,12 +315,12 @@ export default {
             this.dialogFormVisible = false;
             this.getList();
             this.$notify({
-              title: '成功',
-              message: '创建成功',
-              type: 'success',
+              title: "成功",
+              message: "创建成功",
+              type: "success",
               duration: 2000
             });
-          })
+          });
         } else {
           return false;
         }
@@ -308,9 +342,9 @@ export default {
             this.dialogFormVisible = false;
             this.getList();
             this.$notify({
-              title: '成功',
-              message: '创建成功',
-              type: 'success',
+              title: "成功",
+              message: "创建成功",
+              type: "success",
               duration: 2000
             });
           });
@@ -331,5 +365,5 @@ export default {
       };
     }
   }
-}
+};
 </script>
--- webapp/src/views/admin/menu/index.vue
+++ webapp/src/views/admin/menu/index.vue
@@ -1,112 +1,117 @@
 <template>
-<div class="app-container calendar-list-container">
-  <div class="filter-container">
-     <el-button-group>
-    <el-button type="primary" v-if="menuManager_btn_add" icon="plus" @click="handlerAdd">添加</el-button>
-    <el-button type="primary" v-if="menuManager_btn_edit" icon="edit" @click="handlerEdit">编辑</el-button>
-    <el-button type="primary" v-if="menuManager_btn_del" icon="delete" @click="handleDelete">删除</el-button>
-  </el-button-group>
-  </div>
+  <div class="app-container calendar-list-container">
+    <div class="filter-container">
+      <el-button-group>
+        <el-button type="primary" v-if="menuManager_btn_add" icon="plus" @click="handlerAdd">添加</el-button>
+        <el-button type="primary" v-if="menuManager_btn_edit" icon="edit" @click="handlerEdit">编辑</el-button>
+        <el-button type="primary" v-if="menuManager_btn_del" icon="delete" @click="handleDelete">删除</el-button>
+      </el-button-group>
+    </div>
 
-<el-row>
-  <el-col :span="8" style='margin-top:15px;'>
-    <el-input
-      placeholder="输入关键字进行过滤"
-      v-model="filterText">
-    </el-input>
-    <el-tree
-      class="filter-tree"
-      :data="treeData"
-      node-key="id"
-      highlight-current
-      :props="defaultProps"
-      :filter-node-method="filterNode"
-      ref="menuTree"
-      @node-click="getNodeData"
-      default-expand-all
-      >
-    </el-tree>
-  </el-col>
-  <el-col :span="16" style='margin-top:15px;'>
-     <el-card class="box-card">
-    <el-form :label-position="labelPosition" label-width="80px" :model="form" ref="form">
-      <el-form-item label="路径编码" prop="code">
-          <el-input v-model="form.code" :disabled="formEdit" placeholder="请输入路径编码"></el-input>
-      </el-form-item>
-          <el-form-item label="标题" prop="title">
-          <el-input v-model="form.title" :disabled="formEdit"  placeholder="请输入标题"></el-input>
-      </el-form-item>
-          <el-form-item label="父级节点" prop="parentId">
-          <el-input v-model="form.parentId" :disabled="formEdit" placeholder="请输入父级节点" readonly></el-input>
-      </el-form-item>
-      <el-form-item label="图标" prop="icon">
-          <el-input v-model="form.icon" :disabled="formEdit" placeholder="请输入图标"></el-input>
-      </el-form-item>
-          <el-form-item label="资源路径" prop="href">
-          <el-input v-model="form.href" :disabled="formEdit" placeholder="请输入资源路径"></el-input>
-      </el-form-item>
-      <el-form-item label="类型" prop="type">
-         <el-select class="filter-item" v-model="form.type"  :disabled="formEdit"  placeholder="请输入资源请求类型">
-          <el-option v-for="item in  typeOptions" :key="item" :label="item" :value="item"> </el-option>
-        </el-select>
-      </el-form-item>
-      <el-form-item label="排序" prop="orderNum">
-          <el-input v-model="form.orderNum" :disabled="formEdit" placeholder="请输入排序"></el-input>
-      </el-form-item>
-      <el-form-item label="描述"   prop="description">
-          <el-input v-model="form.description" :disabled="formEdit" placeholder="请输入描述"></el-input>
-      </el-form-item>
-       <el-form-item label="前端组件"   prop="attr1">
-          <el-input v-model="form.attr1" :disabled="formEdit" placeholder="请输入描述"></el-input>
-      </el-form-item>
-       <el-form-item v-if="formStatus == 'update'">
-        <el-button type="primary" @click="update">更新</el-button>
-        <el-button @click="onCancel">取消</el-button>
-      </el-form-item>
-      <el-form-item v-if="formStatus == 'create'">
-        <el-button type="primary" @click="create">保存</el-button>
-        <el-button @click="onCancel">取消</el-button>
-      </el-form-item>
-    </el-form>
-     </el-card>
-    <el-card class="box-card">
-        <span>按钮或资源</span>
-      <menu-element :menuId='currentId' ref="menuElement"></menu-element>
-    </el-card>
-  </el-col>
-</el-row>
+    <el-row :gutter="20">
+      <el-col :span="6" style="margin-top:15px;">
+        <el-input placeholder="输入关键字进行过滤" v-model="filterText"></el-input>
+        <el-tree
+          class="filter-tree"
+          :data="treeData"
+          node-key="id"
+          highlight-current
+          :props="defaultProps"
+          :filter-node-method="filterNode"
+          ref="menuTree"
+          @node-click="getNodeData"
+          default-expand-all
+        ></el-tree>
+      </el-col>
+      <el-col :span="18" style="margin-top:15px;">
+        <el-card class="box-card" shadow="never">
+          <el-form :label-position="labelPosition" label-width="80px" :model="form" ref="form">
+            <el-form-item label="路径编码" prop="code">
+              <el-input v-model="form.code" :disabled="formEdit" placeholder="请输入路径编码"></el-input>
+            </el-form-item>
+            <el-form-item label="标题" prop="title">
+              <el-input v-model="form.title" :disabled="formEdit" placeholder="请输入标题"></el-input>
+            </el-form-item>
+            <el-form-item label="父级节点" prop="parentId">
+              <el-input v-model="form.parentId" :disabled="formEdit" placeholder="请输入父级节点" readonly></el-input>
+            </el-form-item>
+            <el-form-item label="图标" prop="icon">
+              <el-input v-model="form.icon" :disabled="formEdit" placeholder="请输入图标"></el-input>
+            </el-form-item>
+            <el-form-item label="资源路径" prop="href">
+              <el-input v-model="form.href" :disabled="formEdit" placeholder="请输入资源路径"></el-input>
+            </el-form-item>
+            <el-form-item label="类型" prop="type">
+              <el-select
+                class="filter-item"
+                v-model="form.type"
+                :disabled="formEdit"
+                placeholder="请输入资源请求类型"
+              >
+                <el-option v-for="item in  typeOptions" :key="item" :label="item" :value="item"></el-option>
+              </el-select>
+            </el-form-item>
+            <el-form-item label="排序" prop="orderNum">
+              <el-input v-model="form.orderNum" :disabled="formEdit" placeholder="请输入排序"></el-input>
+            </el-form-item>
+            <el-form-item label="描述" prop="description">
+              <el-input v-model="form.description" :disabled="formEdit" placeholder="请输入描述"></el-input>
+            </el-form-item>
+            <el-form-item label="前端组件" prop="attr1">
+              <el-input v-model="form.attr1" :disabled="formEdit" placeholder="请输入描述"></el-input>
+            </el-form-item>
+            <el-form-item v-if="formStatus == 'update'">
+              <el-button type="primary" @click="update">更新</el-button>
+              <el-button @click="onCancel">取消</el-button>
+            </el-form-item>
+            <el-form-item v-if="formStatus == 'create'">
+              <el-button type="primary" @click="create">保存</el-button>
+              <el-button @click="onCancel">取消</el-button>
+            </el-form-item>
+          </el-form>
+        </el-card>
+        <el-card class="box-card" shadow="never" style="margin-top:15px;">
+          <span>按钮或资源</span>
+          <menu-element :menuId="currentId" ref="menuElement"></menu-element>
+        </el-card>
+      </el-col>
+    </el-row>
   </div>
 </template>
 
 <script>
 import {
-  fetchTree, getObj, addObj, delObj, putObj
-} from 'api/admin/menu/index';
-import { mapGetters } from 'vuex';
+  fetchTree,
+  getObj,
+  addObj,
+  delObj,
+  putObj
+} from "api/admin/menu/index";
+import { mapGetters } from "vuex";
 export default {
-  name: 'menu',
+  name: "menu",
   components: {
-    'menu-element': () => import('./components/element')
+    "menu-element": () => import("./components/element")
   },
   data() {
     return {
-      filterText: '',
+      filterText: "",
       list: null,
       total: null,
       formEdit: true,
       formAdd: true,
-      formStatus: '',
+      formStatus: "",
       showElement: false,
-      typeOptions: ['menu', 'dirt'],
+      typeOptions: ["menu", "dirt"],
       listQuery: {
         name: undefined
       },
       treeData: [],
       defaultProps: {
-        children: 'children',
-        label: 'title'
+        children: "children",
+        label: "title"
       },
-      labelPosition: 'right',
+      labelPosition: "right",
       form: {
         code: undefined,
         title: undefined,
@@ -124,7 +129,7 @@ export default {
       menuManager_btn_add: false,
       menuManager_btn_edit: false,
       menuManager_btn_del: false
-    }
+    };
   },
   watch: {
     filterText(val) {
@@ -133,14 +138,12 @@ export default {
   },
   created() {
     this.getList();
-    this.menuManager_btn_add = this.elements['menuManager:btn_add'];
-    this.menuManager_btn_del = this.elements['menuManager:btn_del'];
-    this.menuManager_btn_edit = this.elements['menuManager:btn_edit'];
+    this.menuManager_btn_add = this.elements["menuManager:btn_add"];
+    this.menuManager_btn_del = this.elements["menuManager:btn_del"];
+    this.menuManager_btn_edit = this.elements["menuManager:btn_edit"];
   },
   computed: {
-    ...mapGetters([
-      'elements'
-    ])
+    ...mapGetters(["elements"])
   },
   methods: {
     getList() {
@@ -154,7 +157,7 @@ export default {
     },
     getNodeData(data) {
       if (!this.formEdit) {
-        this.formStatus = 'update';
+        this.formStatus = "update";
       }
       getObj(data.id).then(response => {
         this.form = response.data;
@@ -167,28 +170,28 @@ export default {
     handlerEdit() {
       if (this.form.id) {
         this.formEdit = false;
-        this.formStatus = 'update';
+        this.formStatus = "update";
       }
     },
     handlerAdd() {
       this.resetForm();
       this.formEdit = false;
-      this.formStatus = 'create';
+      this.formStatus = "create";
     },
     handleDelete() {
-      this.$confirm('此操作将永久删除, 是否继续?', '提示', {
-        confirmButtonText: '确定',
-        cancelButtonText: '取消',
-        type: 'warning'
+      this.$confirm("此操作将永久删除, 是否继续?", "提示", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "warning"
       }).then(() => {
         delObj(this.currentId).then(() => {
           this.getList();
           this.resetForm();
           this.onCancel();
           this.$notify({
-            title: '成功',
-            message: '删除成功',
-            type: 'success',
+            title: "成功",
+            message: "删除成功",
+            type: "success",
             duration: 2000
           });
         });
@@ -198,9 +201,9 @@ export default {
       putObj(this.form.id, this.form).then(() => {
         this.getList();
         this.$notify({
-          title: '成功',
-          message: '更新成功',
-          type: 'success',
+          title: "成功",
+          message: "更新成功",
+          type: "success",
           duration: 2000
         });
       });
@@ -209,16 +212,16 @@ export default {
       addObj(this.form).then(() => {
         this.getList();
         this.$notify({
-          title: '成功',
-          message: '创建成功',
-          type: 'success',
+          title: "成功",
+          message: "创建成功",
+          type: "success",
           duration: 2000
         });
       });
     },
     onCancel() {
       this.formEdit = true;
-      this.formStatus = '';
+      this.formStatus = "";
     },
     resetForm() {
       this.form = {
@@ -234,6 +237,32 @@ export default {
       };
     }
   }
-}
+};
 </script>
+<style lang="scss" scoped>
+.app-container {
+  .el-input {
+    
+    input.el-input__inner {
+      background: #e7e7e7 !important;
+      border: none !important;
+      border-radius: inherit !important;
+    }
+  }
+
+  .el-tree {
+    cursor: default;
+    background: #f6f6f6;
+    border: none;
+    height: auto;
+  }
+  .el-card {
+    border: 1px solid #eaeff3;
+    border-radius: 4px;
+    background-color: #fff;
+    box-shadow: none;
+  }
+}
+</style>
+
 
